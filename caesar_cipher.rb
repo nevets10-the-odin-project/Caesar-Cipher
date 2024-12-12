e1ea85ed1722f154e1bc3cffb0d8f29e827ca83e
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-11 19:29:19 -0800

    Write function
    Not the most atomic of commits, but what are ya gonna do?

---
+++ caesar_cipher.rb
@@ -0,0 +1,20 @@
+def caesar_cipher(string, shift)
+  str_arr = string.split("")
+  new_arr = []
+
+  str_arr.each do |char|
+    unless char.between?("0", "z") 
+      new_arr << char.chr
+      next
+    end
+
+    shift.times do
+      char.succ!
+    end
+    new_arr << char.chr
+  end
+
+  new_arr.join
+end
+
+puts caesar_cipher("I'll mail you a grilled cheese sandwich!", 10)
